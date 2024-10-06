4cd213b53ea7f47e9b97d0c2c96f15e8f9b7b629
dimaslanjaka <dimaslanjaka@gmail.com>
2024-10-07 04:41:27 +0700

    feat: clickable element helper

--- themes/hexo-theme-flowbite/src/index.ts
+++ themes/hexo-theme-flowbite/src/index.ts
@@ -1,3 +1,4 @@
+import { initClickable } from "./clickable";
 import initFancybox from "./fancybox";
 import { highlightMain, initClipBoard } from "./highlight";
 import initToc from "./toc";
@@ -7,14 +8,17 @@ document.addEventListener("DOMContentLoaded", () => {
   initClipBoard();
   initFancybox();
   initToc();
+  initClickable();
 
   // layout/partials/nav.njk
   const mobileMenuButton = document.querySelector('button[aria-controls="mobile-menu"]');
   const mobileMenu = document.getElementById("mobile-menu");
-  if (!mobileMenu.classList.contains("hidden")) {
-    mobileMenu.classList.add("hidden");
+  if (mobileMenuButton && mobileMenu) {
+    if (!mobileMenu.classList.contains("hidden")) {
+      mobileMenu.classList.add("hidden");
+    }
+    mobileMenuButton.addEventListener("click", () => {
+      mobileMenu.classList.toggle("hidden");
+    });
   }
-  mobileMenuButton.addEventListener("click", () => {
-    mobileMenu.classList.toggle("hidden");
-  });
 });
