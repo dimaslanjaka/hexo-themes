b8c9764c3bb961945298ffac45a4a76285142cb7
dimaslanjaka <dimaslanjaka@gmail.com>
2024-10-08 15:31:52 +0700

    feat: mini loading animation

--- themes/hexo-theme-flowbite/src/loader.ts
+++ themes/hexo-theme-flowbite/src/loader.ts
@@ -1,6 +1,8 @@
 export default function initloader() {
   window.addEventListener("load", function () {
     const preloader = document.getElementById("preloader");
+    const preloaderMini = document.getElementById("preloader-mini");
     if (preloader) preloader.style.display = "none";
+    if (preloaderMini) preloaderMini.style.display = "none";
   });
 }
