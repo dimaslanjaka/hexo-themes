301dc3d595d053a3d970f22f82147149e2f32fdf
dimaslanjaka <dimaslanjaka@gmail.com>
2024-10-10 03:12:09 +0700

    update site Thu Oct 10 2024 03:12:07 GMT+0700 (Western Indonesia Time)

--- hexo-theme-flowbite/js/script.js
+++ hexo-theme-flowbite/js/script.js
@@ -61385,8 +61385,16 @@
             preloaderMini.style.display = "none";
     }
 
+    /**
+     * Initializes the navigation menu, handling both mobile and desktop interactions.
+     *
+     * - Toggles the mobile menu visibility when the corresponding button is clicked.
+     * - Handles submenu interactions for desktop, showing and hiding submenus on mouse enter and leave.
+     * - Handles submenu toggling for mobile, allowing users to click on links to show/hide submenus.
+     *
+     * @returns This function does not return a value.
+     */
     function initNavigationMenu() {
-        // layout/partials/nav.njk
         var mobileMenuButton = document.querySelector('button[aria-controls="mobile-menu"]');
         var mobileMenu = document.getElementById("mobile-menu");
         if (mobileMenuButton && mobileMenu) {
@@ -61406,19 +61414,19 @@
                     var submenu = item.querySelector(".absolute");
                     if (submenu)
                         submenu.classList.remove("hidden");
-                    if (icon)
+                    if (icon) {
                         icon.classList.remove("fa-caret-down");
-                    if (icon)
                         icon.classList.add("fa-caret-up");
+                    }
                 });
                 item.addEventListener("mouseleave", function () {
                     var submenu = item.querySelector(".absolute");
                     if (submenu)
                         submenu.classList.add("hidden");
-                    if (icon)
+                    if (icon) {
                         icon.classList.remove("fa-caret-up");
-                    if (icon)
                         icon.classList.add("fa-caret-down");
+                    }
                 });
             });
             // Handle submenu toggle for mobile
@@ -61460,20 +61468,25 @@
                 });
             });
         }
-        // Function to close all open submenus
-        function closeAllSubmenus(navItems) {
-            navItems.forEach(function (item) {
-                var submenu = item.querySelector(".absolute");
-                var icon = item.querySelector("i");
-                if (submenu && !submenu.classList.contains("hidden")) {
-                    submenu.classList.add("hidden");
-                    if (icon) {
-                        icon.classList.remove("fa-caret-up");
-                        icon.classList.add("fa-caret-down");
-                    }
+    }
+    /**
+     * Closes all open submenus and resets their icons to indicate closed state.
+     *
+     * @param navItems - A NodeList of navigation items containing submenus.
+     * @returns This function does not return a value.
+     */
+    function closeAllSubmenus(navItems) {
+        navItems.forEach(function (item) {
+            var submenu = item.querySelector(".absolute");
+            var icon = item.querySelector("i");
+            if (submenu && !submenu.classList.contains("hidden")) {
+                submenu.classList.add("hidden");
+                if (icon) {
+                    icon.classList.remove("fa-caret-up");
+                    icon.classList.add("fa-caret-down");
                 }
-            });
-        }
+            }
+        });
     }
 
     function initToc() {
