a5e1ebcec630f2fd4e4f2c3b1ef1dd3af35d4fd3
dimaslanjaka <dimaslanjaka@gmail.com>
2024-10-25 11:40:34 +0700

    fix: `.each` is not function

--- src/scripts/post/labels.ts
+++ src/scripts/post/labels.ts
@@ -9,14 +9,22 @@ export function getKeywords(page: HexoPageSchema) {
   const results = [] as string[];
   if (page.keywords && Array.isArray(page.keywords)) return page.keywords;
   if (page.tags) {
-    page.tags.each((label: Label) => {
-      results.push(label.name);
-    });
+    if (typeof page.tags.each === "function") {
+      page.tags.each((label: Label) => {
+        results.push(label.name);
+      });
+    } else if (Array.isArray(page.tags)) {
+      results.push(...page.tags);
+    }
   }
   if (page.categories) {
-    page.categories.each((label: Label) => {
-      results.push(label.name);
-    });
+    if (typeof page.categories.each === "function") {
+      page.categories.each((label: Label) => {
+        results.push(label.name);
+      });
+    } else if (Array.isArray(page.categories)) {
+      results.push(...page.categories);
+    }
   }
   return results;
 }
