3e98c188121c1019143659548d004d7354942414
dimaslanjaka <dimaslanjaka@gmail.com>
2026-05-13 09:07:13 +0700

    refactor(types): redefine HexoLocalsData and HexoRenderData interfaces

--- src/types/post.d.ts
+++ src/types/post.d.ts
@@ -1,4 +1,27 @@
-import { HexoLocalsData } from "hexo-renderers";
+interface HexoLocalsData {
+  [key: string]: any;
+  page: {
+    [key: string]: any;
+    path: string;
+  };
+  path: string;
+  url: string;
+  config: Hexo__default["config"];
+  theme: Record<string, any>;
+  layout: string;
+  env: any;
+  view_dir: string;
+  site: Record<string, any>;
+  cache?: boolean;
+  /** absolute path source post markdown */
+  full_source?: string;
+  tags?: any;
+  categories?: any;
+}
+interface HexoRenderData {
+  text?: string;
+  path?: string;
+}
 
 export type HexoPageSchema = import("hexo/dist/types").PageSchema &
   import("hexo/dist/types").PostSchema &
